20b4eaca456ce6efb8df16eeca455277f8f73dfc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-07 08:23:44 -0300

    feat: add iam role policy

--- iam.tf
+++ iam.tf
@@ -16,31 +16,72 @@ resource "aws_iam_role" "ecr_role" {
   name = "ecr_role"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Principal": {
-                "Federated": "arn:aws:iam::794038230254:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": [
-                        "sts.amazonaws.com"
-                    ]
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": [
-                        "repo:viniciusferreira7/05-nest-clean:ref:refs/heads/main"
-                    ]
-                }
-            }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::794038230254:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:viniciusferreira7/05-nest-clean:ref:refs/heads/main"
+            ]
+          }
         }
+      }
     ]
-})
+  })
+
+  #  aws_iam_role_policy {
+  #   name = "my_inline_policy"
+
+  #   policy = jsonencode({
+  #     Version = "2012-10-17"
+  #     Statement = [
+  #       {
+  #         Action   = ["ec2:Describe*"]
+  #         Effect   = "Allow"
+  #         Resource = "*"
+  #       },
+  #     ]
+  #   })
+  # }
 
   tags = {
     IAC = "True"
   }
+}
+
+resource "aws_iam_role_policy" "ecr-app-permission" {
+  name = "ecr-app-permission"
+  role = aws_iam_role.ecr_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "Statement1"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
