f3ddeaae5c40f15dec3bc36a983236cf23766a4a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-30 06:27:37 -0300

    feat: add s3 bucket to save terraform state

--- main.tf
+++ main.tf
@@ -11,3 +11,16 @@ provider "aws" {
   region  = "us-east-2"
   profile = var.profile
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "05-nest-clean-infra"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
